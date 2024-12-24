431961d13686fe7770c2449cf0cc0be142aeee37
LalitMahale <lalitmahale121@gmail.com>
2024-12-24 22:37:40 +0530

    code added

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 import streamlit as st
+import base64
+
+st.set_page_config(layout="wide")
+
+@st.cache_data
+def display_pdf(file):
+    # with open(file,"rb") as f:
+    base64_pdf = base64.b64encode(file.read()).decode("utf-8")
+    
+    display = f"<iframe src='data:application/pdf;base64,{base64_pdf}' width = '100%' height = '600' type = 'application/pdf'></iframe>"
+
+    st.markdown(display, unsafe_allow_html=True)
 
 
-st.markdown("<h2 style = 'text align:center'>All in one Summarizer</h2>")
+def main():
+    st.title("All in 1 Document summarizer")
 
-file = st.file_uploader("upload files")
-if file != None:
-    with open(file,'rb') as f:
-        data = f.read()
-    st.write(data)
+    file = st.file_uploader("Upload file",type=['pdf'])
+    if file != None:
+        if st.button("Summarize"):
+            col1,col2 = st.columns(2)
+            with col1:
+                display_pdf(file)
+            with col2:
+                pass
+
+
+
+
+main()
+    
