6bccb6058db519477d39ef37b4de99f51cfda130
LalitMahale <lalitmahale121@gmail.com>
2024-12-24 18:28:01 +0530

    basic code added

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 
 st.markdown("<h2 style = 'text align:center'>All in one Summarizer</h2>")
+
+file = st.file_uploader("upload files")
+if file != None:
+    with open(file,'rb') as f:
+        data = f.read()
+    st.write(data)
